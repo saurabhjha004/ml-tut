4b5a107ba689f49e3c2e0e3eb3cf588bc9ea96cd
saurabhjha004 <saurabhdkmjha@gmail.com>
2024-05-17 21:44:47 +0530

    code added

--- numpy3.py
+++ numpy3.py
@@ -10,12 +10,14 @@ print(z)
 z1 = np.add(x, y)
 print(z1)
 
+#making own ufunc function for adding two arrays
 def myadd(x, y):
   return x+y
 myadd = np.frompyfunc(myadd, 2, 1)
 print(myadd([1, 2, 3, 4], [5, 6, 7, 8]))
 print(type(np.add))
 print(type(np.concatenate))
+#checking whether the made function is ufunc or not
 if type(np.add) == np.ufunc:
   print('add is ufunc')
 else:
