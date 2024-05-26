354aec1d1e9bfa7f3bf2d39408bf252327b29f86
saurabhjha004 <saurabhdkmjha@gmail.com>
2024-05-26 21:58:03 +0530

    code added

--- pandas1.py
+++ pandas1.py
@@ -6,3 +6,17 @@ mydataset = {
 myvar = pd.DataFrame(mydataset)
 print(myvar)
 print(pd.__version__) #checking pandas version
+a = [1, 7, 2]
+myvar = pd.Series(a)
+print(myvar)
+print(myvar[0])
+myvar = pd.Series(a, index = ["x", "y", "z"])
+print(myvar)
+print(myvar["y"])
+calories = {"day1": 420, "day2": 380, "day3": 390}
+myvar = pd.Series(calories)
+print(myvar)
+myvar = pd.Series(calories, index = ["day1", "day2"])
+print(myvar)
+myvar = pd.DataFrame(data)
+print(myvar)
